ef5e76fa03e9601ed8d373fcb7ce2eb8ea35b748
Sen VU <sen@vanulden.nl>
2025-06-04 13:27:26 +0200

    change

--- Assets/scripts/BiomeMusicManager.cs
+++ Assets/scripts/BiomeMusicManager.cs
@@ -1,30 +1,23 @@
 using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class BiomeMusicManager : MonoBehaviour
-{
-    public enum Biome
-    {
-        Ice,
-        Lava,
-        Space
-        // Add more biomes here
+public class BiomeMusicManager : MonoBehaviour {
+    public enum BiomeType { 
+        NONE,
+        ICE,
+        LAVA,
+        SPACE
     }
 
-    [System.Serializable]
-    public class BiomeMusic
-    {
-        public Biome biome;
-        public AudioClip musicClip;
-    }
-
-    public BiomeMusic[] biomeTracks;
-    public float fadeDuration = 1.5f;
+    [SerializeField] BiomeType startBiome;
+    [SerializeField] PseudoDictionary<BiomeType, AudioClip> biomeTracks;
+    [SerializeField] float fadeDuration = 1.5f;
 
     private AudioSource audioSourceA;
     private AudioSource audioSourceB;
     private AudioSource currentSource;
-    private Biome currentBiome;
+    private BiomeType currentBiome;
     private int currentBiomeIndex = 0;
 
     void Awake()
@@ -44,7 +37,7 @@ public class BiomeMusicManager : MonoBehaviour
     void Start()
     {
         // Immediately play Ice biome
-        currentBiome = Biome.Ice;
+        currentBiome = startBiome;
         currentBiomeIndex = 0;
         TransitionToBiome(currentBiome);
     }
@@ -53,27 +46,28 @@ public class BiomeMusicManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            currentBiomeIndex = (currentBiomeIndex + 1) % System.Enum.GetNames(typeof(Biome)).Length;
-            Biome nextBiome = (Biome)currentBiomeIndex;
+            currentBiomeIndex = (currentBiomeIndex + 1) % System.Enum.GetNames(typeof(BiomeType)).Length;
+            BiomeType nextBiome = (BiomeType)currentBiomeIndex;
             TransitionToBiome(nextBiome);
         }
     }
 
-    public void TransitionToBiome(Biome newBiome)
+    public void TransitionToBiome(BiomeType newBiome)
     {
         if (newBiome == currentBiome) return;
 
         AudioClip newClip = GetClipForBiome(newBiome);
-        if (newClip == null)
-        {
-            Debug.LogWarning($"No clip found for biome {newBiome}");
-            return;
-        }
 
         AudioSource nextSource = (currentSource == audioSourceA) ? audioSourceB : audioSourceA;
-        nextSource.clip = newClip;
-        nextSource.volume = 0f;
-        nextSource.Play();
+        // TODO does this crash?
+        if (newClip != null) {
+            nextSource.clip = newClip;
+            nextSource.volume = 0f;
+            nextSource.Play();
+        } else {
+            nextSource.Stop();
+            nextSource.volume = 0f;
+        }
 
         StartCoroutine(CrossfadeTracks(currentSource, nextSource));
         currentSource = nextSource;
@@ -82,13 +76,10 @@ public class BiomeMusicManager : MonoBehaviour
         Debug.Log($"Switched to biome: {newBiome}");
     }
 
-    private AudioClip GetClipForBiome(Biome biome)
+    private AudioClip GetClipForBiome(BiomeType biome)
     {
-        foreach (var entry in biomeTracks)
-        {
-            if (entry.biome == biome)
-                return entry.musicClip;
-        }
+        if (biomeTracks.ContainsKey(biome))
+            return biomeTracks[biome];
         return null;
     }
 
