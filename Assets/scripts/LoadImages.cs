8c8f865ead5e16995f19f9ed5d8fb175de688d7d
Joris <549108@student.saxion.nl>
2025-06-11 14:26:36 +0200

    gallery yippeee
    yippeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- Assets/scripts/LoadImages.cs
+++ Assets/scripts/LoadImages.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using Unity.Mathematics;
 
 public class LoadImages : MonoBehaviour
 {
@@ -10,26 +11,56 @@ public class LoadImages : MonoBehaviour
     [SerializeField] float maxHeight = 108;
 
     [Header("Displays")]
-    [SerializeField] Image imageDisplay;
+    [SerializeField] private List<Image> images;
 
-    private Texture2D[] allImages;
+    [SerializeField, ReadOnly] private int pageNumber = 0;
 
     void Start()
     {
         GaleryLoader.Load();
 
-        allImages = GaleryLoader.getImages();
-        Debug.Log("amount of images: " + allImages.Length);
+        Debug.Log("amount of images: " + GaleryLoader.getImages().Length);
 
-        Texture2D texture = allImages[2];
-        Transform imageTransform = imageDisplay.GetComponent<Transform>();
-        imageTransform.localScale = new Vector2(Mathf.Clamp(texture.width, 1, maxWidth), Mathf.Clamp(texture.height, 1, maxHeight));
+        UpdatePage();
 
-        imageDisplay.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+        //Transform imageTransform = imageDisplay.GetComponent<Transform>();
+        //Image image = imageTransform.GetComponent<Image>();
+        //image.preserveAspect = true;
+
+
+
+        //imageTransform.localScale = new Vector2(Mathf.Clamp(texture.width, 1, maxWidth), Mathf.Clamp(texture.height, 1, maxHeight));
+
     }
 
-    void Update()
+    public void PreviousPage()
     {
-        
+        pageNumber--;
+        if (pageNumber < 0) pageNumber = 0;
+        UpdatePage();
+    }
+
+    public void NextPage()
+    {
+        pageNumber = math.min(pageNumber + 1, GaleryLoader.loadedImageCount() / images.Count);
+        UpdatePage();
+    }
+
+    private void UpdatePage()
+    {
+        int i = pageNumber * images.Count;
+
+        foreach (Image image in images)
+        {
+            Texture2D texture = GaleryLoader.getImage(i);
+            if (texture != null)
+            {
+                image.gameObject.SetActive(true);
+                image.preserveAspect = true;
+                image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+            }
+            else image.gameObject.SetActive(false);
+            i++;
+        }
     }
 }
